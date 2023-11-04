213b9e597325a34b467e69be68a36923539a0b64
Aaroway <146600984+Aaroway@users.noreply.github.com>
2023-11-04 18:56:53 -0500

    player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,14 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // take the current position
+        // assign it a new position assigning it an x, y, and zed
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
     }
 }
