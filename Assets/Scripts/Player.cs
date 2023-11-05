07c88f1963f2b70c30ae76c4e1a39eaeae0cdb5a
Aaroway <146600984+Aaroway@users.noreply.github.com>
2023-11-05 15:32:01 -0600

    player wrap on x plus speed increase

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3.5f;
+    private float _speed = 6.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +26,21 @@ public class Player : MonoBehaviour
 
         if (transform.position.y >= 0)
         {
-            transform.position = new Vector3(transform.position. x, 0, 0);
+            transform.position = new Vector3(transform.position.x, 0, 0);
         }
         else if (transform.position.y < -3.8f)
         {
-            transform.position = new Vector3(transform.position. x, -3.8f, 0);
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
         }
-        
+        //11.3 on x pos and neg
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
-        
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
+}
