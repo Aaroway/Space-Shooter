d1a5c0267b4cea94a7816e0f42d4b42b7fec9bc9
Aaroway <146600984+Aaroway@users.noreply.github.com>
2023-11-05 15:49:43 -0600

    player code cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,31 +16,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizotalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        transform.Translate(Vector3.right * horizotalInput * _speed * Time.deltaTime);
-        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
-
-        //if player is greater than 0 on y, y = 0
-
-        if (transform.position.y >= 0)
+        CalculateMovement();
+    }
+    
+    void CalculateMovement()
+    {
         {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-        }
-        else if (transform.position.y < -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
-        }
-        //11.3 on x pos and neg
+            float horizotalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
 
-        if (transform.position.x > 11.3f)
-        {
-            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+            transform.Translate(Vector3.right * horizotalInput * _speed * Time.deltaTime);
+            transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+            if (transform.position.y >= 0)
+            {
+                transform.position = new Vector3(transform.position.x, 0, 0);
+            }
+            else if (transform.position.y < -3.8f)
+            {
+                transform.position = new Vector3(transform.position.x, -3.8f, 0);
+            }
+            //11.3 on x pos and neg
+
+            if (transform.position.x > 11.3f)
+            {
+                transform.position = new Vector3(-11.3f, transform.position.y, 0);
+            }
+            else if (transform.position.x < -11.3f)
+            {
+                transform.position = new Vector3(11.3f, transform.position.y, 0);
+            }
         }
-        else if (transform.position.x < -11.3f)
-        {
-            transform.position = new Vector3(11.3f, transform.position.y, 0);
-        }
+
     }
 }
