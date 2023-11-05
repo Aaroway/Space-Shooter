d103c1e2938d4c4f3d9666291f06eb53e81f28d4
Aaroway <146600984+Aaroway@users.noreply.github.com>
2023-11-04 20:09:44 -0500

    vertical stop

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,6 +21,18 @@ public class Player : MonoBehaviour
 
         transform.Translate(Vector3.right * horizotalInput * _speed * Time.deltaTime);
         transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //if player is greater than 0 on y, y = 0
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position. x, 0, 0);
+        }
+        else if (transform.position.y < -3.8f)
+        {
+            transform.position = new Vector3(transform.position. x, -3.8f, 0);
+        }
+        
+        }
         
     }
-}
