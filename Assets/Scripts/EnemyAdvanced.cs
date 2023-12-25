08f7db785ce0e1d618bebd6dfb8de8340734a1d6
Aaroway <146600984+Aaroway@users.noreply.github.com>
2023-12-24 19:05:38 -0600

    new scripts

--- Assets/Scripts/EnemyAdvanced.cs
+++ Assets/Scripts/EnemyAdvanced.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class EnemyAdvanced : MonoBehaviour
 {
+    [SerializeField]
+    private float _sideSpeed = 3f;
+    [SerializeField]
+    private float _enemySpeed = 5.0f;
+    public int scoreValue = 25;
+    private Player _player;
+    private Animator _anim;
+    private AudioClip _explosionSoundClip;
+    private AudioSource _audioSource;
+    private GameObject _laserPrefab;
+    private float _fireRate = 3.0f;
+    private float _canFire = -1f; //x= -13 to 13, y= 4.5
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _uiManager = GameObject.Find("Canvas").GetComponent<UI_Manager>();
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_player == null)
+        {
+            Debug.LogError("player is null");
+        }
+
+        _anim = GetComponent<Animator>();
+
+        if (_anim == null)
+        {
+            Debug.LogError("Animator is null");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement();
+
+        EnemyFire();
     }
 }
---
+++ Assets/Scripts/SpawnMan2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMan2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject[] _powerUps;
+    [SerializeField]
+    private GameObject _enemyContainer;
+    [SerializeField]
+    private GameObject _enemyPrefab2;
+    private int _spawnX = int.MinValue(-13f, 13f);
+
+    private bool _isDead = false;
+
+
+    private float currentSpawnDelay = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        yield return new WaitForSeconds(3.0f);
+        StartSpawning();
+    }
+
+    // Update is called once per frame
+
+
+    public void StartSpawning()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnPowerUpRoutine());
+    }
+
+    public void SpawnEnemyRoutine()
+    {
+        while (!_isDead)
+        {
+            Vector3 spawnPosition = new Vector3(-13f, 13f), 4.5f, 0f);
+        }
+    }
+
+}
